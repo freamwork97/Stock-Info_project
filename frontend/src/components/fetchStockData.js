3ae3e57faeb283dabb8ff09a37e40b70ec25e267
freamwork97 <jsy11123@naver.com>
2023-10-09 20:57:58 +0900

    주가데이터 분리

---
+++ frontend/src/components/fetchStockData.js
@@ -0,0 +1,19 @@
+const fetchStockData = async (searchTerm, setCompanyInfo, setStockChart, setStockPrice) => {
+    try {
+      const response = await fetch(`http://localhost:8000/stock/${searchTerm}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch company info');
+      }
+      const data = await response.json();
+      setCompanyInfo(data);
+      const latestData = data.daily_prices.slice(0, 7); // 차트 그리기 위해
+      setStockChart(latestData);
+      const latestData2 = data.daily_prices.slice(0, 1); // 전일가
+      setStockPrice(latestData2);
+    } catch (error) {
+      console.error('Error fetching company info:', error);
+    }
+  };
+  
+  export default fetchStockData;
+  
