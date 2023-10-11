7f61b81f031ed8240c1c9aa258f6c9740aa80541
freamwork97 <jsy11123@naver.com>
2023-10-12 08:56:04 +0900

    Route 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import SearchResultPage from './pages/SearchResultPage';
+import ChartDetailPage from './pages/ChartDetailPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} /> {/*메인페이지*/}
         <Route path="/search/:searchTerm" element={<SearchResultPage />} /> {/*검색결과*/}
+        <Route path="/chart/:searchTerm" element={<ChartDetailPage />} /> {/*차트상세*/}
       </Routes>
     </Router>
   );
