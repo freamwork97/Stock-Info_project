2cfe5e8b1bd81c53da97fb121e1a49b4c24ede41
freamwork97 <jsy11123@naver.com>
2023-10-12 07:54:35 +0900

    주요 지수

--- backend/main.py
+++ backend/main.py
@@ -82,6 +82,7 @@ def read_company_names(prefix: str):
     company_names = get_company_names(prefix)
     return company_names
 
+# 주요 지수
 @app.get("/key_index")
 def read_key_index():
     data = get_key_index()
