31958332c7e43afb3cc66ce4eeb8dee0bca3af17
freamwork97 <jsy11123@naver.com>
2023-10-12 11:03:46 +0900

    차트 상세페이지

--- frontend/src/pages/ChartDetailPage.js
+++ frontend/src/pages/ChartDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import fetchStockPrice from '../components/fetchLatestClose';
+import fetchStockData from '../components/fetchStockData';
 
 
 function ChartDetailPage() {
@@ -8,6 +9,11 @@ function ChartDetailPage() {
   console.log(searchTerm)
   const [stockprice, setStockPrice] = useState([]);
   console.log(stockprice)
+  const [companyInfo, setCompanyInfo] = useState({});
+  
+  useEffect(() => {
+    fetchStockData(searchTerm, setCompanyInfo);
+  }, [searchTerm]);
 
   useEffect(() => {
     fetchStockPrice(searchTerm, setStockPrice);
@@ -15,9 +21,19 @@ function ChartDetailPage() {
   }, [searchTerm]);
 
   return (
-    <div>
-      <div className="chart-area">상세한 차트 영역</div>
-      <div className="indicators">보조지표 설정 영역</div>
+    <div className='container mt-5'>
+      <div className='card'>
+        <div className="chart-area">
+          <h2 className='mt-3 mx-3'>
+            차트<br></br>
+            {companyInfo.company} 
+            ({companyInfo.code}) 
+          </h2>
+        </div>
+      </div>
+      <div className='card mt-4'>
+        <div className="indicators">보조지표 설정 영역</div>
+      </div>
     </div>
   );
 }
