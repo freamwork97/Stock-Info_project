97da56fdfab5f59c88365e69a49530b468e7d944
freamwork97 <jsy11123@naver.com>
2023-10-16 09:41:43 +0900

    echart Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import SearchResultPage from './pages/SearchResultPage';
 import ChartDetailPage from './pages/ChartDetailPage';
+import BarChart from './test/etest';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<MainPage />} /> {/*메인페이지*/}
         <Route path="/search/:searchTerm" element={<SearchResultPage />} /> {/*검색결과*/}
         <Route path="/chart/:searchTerm" element={<ChartDetailPage />} /> {/*차트상세*/}
+        <Route path="/test" element={<BarChart />} /> {/*EchartTest*/}
       </Routes>
     </Router>
   );
