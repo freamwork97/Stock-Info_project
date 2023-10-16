d5067f502e89157de0f53f6931ae467b4caf3826
freamwork97 <jsy11123@naver.com>
2023-10-17 08:58:09 +0900

    코드 정리

--- frontend/src/pages/ChartDetailPage.js
+++ frontend/src/pages/ChartDetailPage.js
@@ -45,9 +45,8 @@ function ChartDetailPage() {
               width="30" 
               height="30" 
               fill="currentColor" 
-              class="bi bi-chevron-compact-left" 
+              className="bi bi-chevron-compact-left mb-2" 
               viewBox="0 0 16 16"
-              className='mb-2'
               color='gray'>
             <path fill-rule="evenodd" 
             d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
