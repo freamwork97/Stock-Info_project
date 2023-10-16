4a21669932ec841d86cbe25d61c852bfd85113e3
freamwork97 <jsy11123@naver.com>
2023-10-16 14:37:05 +0900

    차트 상세페이지 수정

--- frontend/src/components/drawChart3.js
+++ frontend/src/components/drawChart3.js
@@ -1,76 +1,45 @@
-// 캔들스틱
-export const newDrawCandlestickChart = (priceData, canvasRef) => {
-  const canvas = canvasRef.current;
-  const ctx = canvas.getContext('2d');
-  const infoBox = document.getElementById('info-box');
+import React, { useEffect } from 'react';
+import * as echarts from 'echarts';
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  const numDataPoints = priceData.시가.length;
-  const candleWidth = 15;
-  const xSpacing = 1;
-  const maxPrice = Math.max(...priceData.고가);
-  const minPrice = Math.min(...priceData.저가);
-  let prevMonth = null;
-
-  canvas.addEventListener('mousemove', handleMouseMove);
-
-  function handleMouseMove(event) {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-
-    const dataIndex = Math.floor(x / (candleWidth + xSpacing));
-    if (dataIndex >= 0 && dataIndex < numDataPoints) {
-      const date = priceData.날짜[dataIndex];
-      const high = priceData.고가[dataIndex];
-      const low = priceData.저가[dataIndex];
-      const open = priceData.시가[dataIndex];
-      const close = priceData.종가[dataIndex];
-      const trade = priceData.거래량[dataIndex];
-
-      const infoText = `날짜: ${date} 시가: ${open} 종가: ${close} 저가: ${low} 고가: ${high} 거래량: ${trade}`;
-
-      infoBox.innerText = infoText;
-    }
-  }
-
-  for (let i = 0; i < numDataPoints; i++) {
-    const date = priceData.날짜[i];
-    const month = date.substring(4, 6);
-
-    if (month !== prevMonth) {
-      const monthStart = date.substring(0, 6);
-      ctx.fillStyle = 'black';
-      ctx.font = '12px Arial';
-      ctx.fillText(monthStart, i * (candleWidth + xSpacing), canvas.height - 10);
-      prevMonth = month;
-    }
-
-    const high = priceData.고가[i];
-    const low = priceData.저가[i];
-    const open = priceData.시가[i];
-    const close = priceData.종가[i];
-
-    const xPosition = i * (candleWidth + xSpacing);
-    const yPositionHigh = canvas.height - ((high - minPrice) / (maxPrice - minPrice) * canvas.height);
-    const yPositionLow = canvas.height - ((low - minPrice) / (maxPrice - minPrice) * canvas.height);
-    const yPositionOpen = canvas.height - ((open - minPrice) / (maxPrice - minPrice) * canvas.height);
-    const yPositionClose = canvas.height - ((close - minPrice) / (maxPrice - minPrice) * canvas.height);
-
-    if (close > open) {
-      ctx.fillStyle = 'red';
-    } else {
-      ctx.fillStyle = 'blue';
-    }
-
-    ctx.fillRect(xPosition, yPositionOpen, candleWidth, yPositionClose - yPositionOpen);
-
-    ctx.strokeStyle = ctx.fillStyle;
-    ctx.beginPath();
-    ctx.moveTo(xPosition + candleWidth / 2, yPositionHigh);
-    ctx.lineTo(xPosition + candleWidth / 2, yPositionClose);
-    ctx.moveTo(xPosition + candleWidth / 2, yPositionLow);
-    ctx.lineTo(xPosition + candleWidth / 2, yPositionOpen);
-    ctx.stroke();
-  }
-};
+const CandlestickChart = ({ priceData }) => {
+    useEffect(() => {
+      if (priceData) {
+        const chartDom = document.getElementById('candlestickChart');
+        const myChart = echarts.init(chartDom);
+  
+        const option = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+            },
+          },
+          xAxis: {
+            data: priceData.날짜,
+          },
+          yAxis: {},
+          series: [
+            {
+              type: 'candlestick',
+              data: priceData.시가.map((_, i) => [
+                priceData.시가[i], // 시가
+                priceData.종가[i], // 종가
+                priceData.저가[i], // 저가
+                priceData.고가[i], // 고가
+              ]),
+            },
+          ],
+        };
+  
+        myChart.setOption(option);
+  
+        return () => {
+          myChart.dispose();
+        };
+      }
+    }, [priceData]);
+  
+    return <div id="candlestickChart" style={{ width: '100%', height: '500px' }} />;
+  };
+  
+export default CandlestickChart;
--- frontend/src/pages/ChartDetailPage.js
+++ frontend/src/pages/ChartDetailPage.js
@@ -1,41 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { newDrawCandlestickChart } from '../components/drawChart3';
 import {fetchStockData, fetchLongData} from '../components/fetchStockData';
+import CandlestickChart from '../components/drawChart3';
+
 
 function ChartDetailPage() {
   const { searchTerm } = useParams();
-  const [LongData, setLongData] = useState(null);
-  const canvasRef = useRef(null);
+  const [priceData, setPriceData] = useState(null);
   const [companyInfo, setCompanyInfo] = useState({});
 
   useEffect(() => {
-    if (LongData) {
-      newDrawCandlestickChart(LongData, canvasRef);
-      scrollToRight();
-    }
-  }, [LongData]);
-
-  const scrollToRight = () => {
-    const chartArea = document.getElementById('chart-area');
-    chartArea.scrollLeft = chartArea.scrollWidth;
-  };
-
-  useEffect(() => {
     fetchLongData(searchTerm).then(data => {
-      setLongData(data);
+      setPriceData(data); 
     });
   }, [searchTerm]);
 
   useEffect(() => {
     fetchStockData(searchTerm, setCompanyInfo);
   }, [searchTerm]);
-
-  useEffect(() => {
-    if (LongData) {
-      newDrawCandlestickChart(LongData, canvasRef);
-    }
-  }, [LongData]);
 
   return (
     <div className='container mt-4'>
@@ -46,7 +28,7 @@ function ChartDetailPage() {
       <div className='card mt-4'>
         <div id='chart-area' className="chart-area table-responsive">
           <h2 className='mt-3 mx-3'>
-            <canvas ref={canvasRef} width="4000" height="400" ></canvas>
+            <CandlestickChart priceData={priceData} />
           </h2>
         </div>
         <div id="info-box" className="mt-3 mx-3 mb-2"></div>
