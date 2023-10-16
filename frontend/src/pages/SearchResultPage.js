6bd5d7918f7d9e930d975c09e70e683b811afa6e
freamwork97 <jsy11123@naver.com>
2023-10-17 08:40:43 +0900

    뒤로가기 메인 스타일 변경

--- frontend/src/pages/SearchResultPage.js
+++ frontend/src/pages/SearchResultPage.js
@@ -48,7 +48,19 @@ function SearchResultPage() {
   return (
     <div className="container mt-5">
       <div className='input-group'>
-        <a href='/' className="fs-3 me-2">메인</a>
+        <a href='/' className="fs-3 me-2">
+        <svg xmlns="http://www.w3.org/2000/svg" 
+              width="40" 
+              height="30" 
+              fill="currentColor" 
+              class="bi bi-chevron-compact-left" 
+              viewBox="0 0 16 16"
+              className='mb-2'
+              color='gray'>
+            <path fill-rule="evenodd" 
+            d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
+          </svg>
+        </a>
         <InputButton IB={InputButton} />
       </div>
       <div className="row">
