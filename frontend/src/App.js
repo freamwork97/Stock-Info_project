ea727b25512978d52484f19f3ff040d66d7af795
freamwork97 <jsy11123@naver.com>
2023-10-17 10:37:16 +0900

    투자리포트 게시판

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import SearchResultPage from './pages/SearchResultPage';
 import ChartDetailPage from './pages/ChartDetailPage';
 import BarChart from './test/Etest';
 import CandlestickChart from './test/EChartCandleTEST';
+import InvestmentReportPage from './pages/InvestmentReportPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<MainPage />} /> {/*메인페이지*/}
         <Route path="/search/:searchTerm" element={<SearchResultPage />} /> {/*검색결과*/}
         <Route path="/chart/:searchTerm" element={<ChartDetailPage />} /> {/*차트상세*/}
+        <Route path='/Report' element={<InvestmentReportPage/>}/>
         <Route path="/test.bar" element={<BarChart />} /> {/*EchartTest*/}
         <Route path="/test.candle" element={<CandlestickChart />} /> {/*EchartTest*/}
       </Routes>
---
+++ frontend/src/pages/InvestmentReportPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function InvestmentReportPage(){
+
+
+return (
+    <div>
+        대기중
+    </div>
+    )
+}
+
+export default InvestmentReportPage;
