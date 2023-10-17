aa50eb53b66f518fe30060a68390da007e2f9217
freamwork97 <jsy11123@naver.com>
2023-10-17 15:52:46 +0900

    배경 이미지 제거

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,10 @@ import ChartDetailPage from './pages/ChartDetailPage';
 import BarChart from './test/Etest';
 import CandlestickChart from './test/EChartCandleTEST';
 import InvestmentReportPage from './pages/InvestmentReportPage';
-import background from "./img/background.jpg";
+
 function App() {
-  const containerStyle = {
-    backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    width: '100%',
-  };
-  const containerBlurStyle = {
-    position: 'relative',
-    backdropFilter: 'blur(1px)', // 조절 가능한 흐림 정도
-  };
-  
   return (
-    <div className="container mt-4" style={containerStyle}>
-      <div className="container" style={containerBlurStyle}>
+    <div className="container mt-4">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/">메인</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -47,7 +36,6 @@ function App() {
           <Route path="/test.candle" element={<CandlestickChart />} /> {/*EchartTest*/}
         </Routes>
       </Router>
-    </div>
     </div>
   );
 }
