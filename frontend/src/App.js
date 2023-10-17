824eca5a0b01396dff4090cf9de6cc43d0a5f573
freamwork97 <jsy11123@naver.com>
2023-10-17 14:13:22 +0900

    네비바 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,33 @@ import InvestmentReportPage from './pages/InvestmentReportPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} /> {/*메인페이지*/}
-        <Route path="/search/:searchTerm" element={<SearchResultPage />} /> {/*검색결과*/}
-        <Route path="/chart/:searchTerm" element={<ChartDetailPage />} /> {/*차트상세*/}
-        <Route path='/Report' element={<InvestmentReportPage/>}/> {/*투자리포트*/}
-        <Route path="/test.bar" element={<BarChart />} /> {/*EchartTest*/}
-        <Route path="/test.candle" element={<CandlestickChart />} /> {/*EchartTest*/}
-      </Routes>
-    </Router>
+    <div className="container mt-4">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">홈</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Report">투자리포트</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/test">테스트</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} /> {/*메인페이지*/}
+          <Route path="/search/:searchTerm" element={<SearchResultPage />} /> {/*검색결과*/}
+          <Route path="/chart/:searchTerm" element={<ChartDetailPage />} /> {/*차트상세*/}
+          <Route path='/Report' element={<InvestmentReportPage/>}/> {/*투자리포트*/}
+          <Route path="/test.bar" element={<BarChart />} /> {/*EchartTest*/}
+          <Route path="/test.candle" element={<CandlestickChart />} /> {/*EchartTest*/}
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
