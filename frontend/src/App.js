695b86b030503bf0c53e8bf960a69766385f05be
freamwork97 <jsy11123@naver.com>
2023-10-17 16:27:01 +0900

    네비바

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,13 @@ function App() {
     <div className="container mt-4">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/">메인</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarNav" 
+                aria-controls="navbarNav" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
