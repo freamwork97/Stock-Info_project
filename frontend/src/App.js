98ff023c5c370e6109357a5e99efdf7a26d08687
freamwork97 <jsy11123@naver.com>
2023-10-17 10:37:55 +0900

    투자리포트 게시판

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<MainPage />} /> {/*메인페이지*/}
         <Route path="/search/:searchTerm" element={<SearchResultPage />} /> {/*검색결과*/}
         <Route path="/chart/:searchTerm" element={<ChartDetailPage />} /> {/*차트상세*/}
-        <Route path='/Report' element={<InvestmentReportPage/>}/>
+        <Route path='/Report' element={<InvestmentReportPage/>}/> {/*투자리포트*/}
         <Route path="/test.bar" element={<BarChart />} /> {/*EchartTest*/}
         <Route path="/test.candle" element={<CandlestickChart />} /> {/*EchartTest*/}
       </Routes>
