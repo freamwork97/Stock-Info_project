a60f0b40478a164101749673133f0ccea4caac52
freamwork97 <jsy11123@naver.com>
2023-10-18 10:58:50 +0900

    게시글 조회

--- frontend/src/pages/PostDetailPage.js
+++ frontend/src/pages/PostDetailPage.js
@@ -1,47 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
-const samplePosts = [
-    {
-      id: 1,
-      title: '첫 번째 게시물',
-      author: '작성자1',
-      content: '첫 번째 게시물의 내용입니다.',
-      created_at: '2023-10-04 10:00:00'
-    },
-    {
-      id: 2,
-      title: '두 번째 게시물',
-      author: '작성자2',
-      content: '두 번째 게시물의 내용입니다.',
-      created_at: '2023-10-04 11:00:00'
-    },
-    {
-      id: 3,
-      title: '세 번째 게시물',
-      author: '작성자3',
-      content: '세 번째 게시물의 내용입니다.',
-      created_at: '2023-10-04 12:00:00'
-    },
-    {
-      id: 4,
-      title: '네 번째 게시물',
-      author: '작성자4',
-      content: '샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플샘플',
-      created_at: '2023-10-04 12:00:00'
-    },
-    {
-      id: 5,
-      title: '세 번째 게시물',
-      author: '작성자3',
-      content: '세 번째 게시물의 내용입니다.',
-      created_at: '2023-10-04 12:00:00'
-    }
-]
 
 function PostDetailPage() {
   let { id } = useParams();
-  const post = samplePosts.find(post => post.id.toString() === id);
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/post/${id}`);
+        const data = await response.json();
+        setPost(data[0]); // 첫 번째 요소를 가져옴
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [id]); // id가 변경될 때마다 다시 불러옴
 
   if (!post) {
     return <div>게시물을 찾을 수 없습니다.</div>;
