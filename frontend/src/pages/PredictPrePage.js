60a7ca9963b8c5d09560a76dc045450e26a8bdb7
freamwork97 <jsy11123@naver.com>
2023-10-27 09:20:40 +0900

    설명문 및 경고문 추가

--- frontend/src/pages/PredictPrePage.js
+++ frontend/src/pages/PredictPrePage.js
@@ -6,9 +6,18 @@ function PredictPrePage() {
   return (
     <div className="container p-5">
       <div className='w-25 mx-auto'>
-        <InputButton IB={InputButton} toPage={'test'}/>
+        <div className="mb-3">
+          <InputButton IB={InputButton} toPage={'test'}/>
+        </div>
+        <p className='mt-4 m-3'>예측을 원하는 종목명을 검색하세요.</p>
+        <div className="alert alert-warning" role="alert">
+          투자는 본인의 선택입니다.<br /> 
+          재미삼아 만들어본 페이지입니다.<br />
+          예측 결과는 정확하지 않습니다.
+        </div>
       </div>
     </div>
+
   );
 }
 export default PredictPrePage;
